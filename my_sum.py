9e44f04774507f51397422b3272037612a88d0c1
Maritza Esparza <maritzaesparza@Maritzas-MacBook-Pro-2.local>
2023-03-01 17:02:32 -0600

    Add some tests and github actions

---
+++ my_sum.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+def test_sum_tuple():
+    assert sum((2, 2, 2)) == 6, "Should be 6"
